d0659e1a938a590ee3e8d23309e2896e9d0ca6b8
Sergio Fernandez <58430563+1mS3r@users.noreply.github.com>
2022-11-04 00:13:14 +0100

    Added terraform final code

---
+++ GCP/Automating Infrastructure on GCloud with Terraform/Terraform Infrastructure Code/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  backend "gcs" {
+    bucket  = "tf-bucket-209716"
+ prefix  = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.55.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+
+module "instances" {
+  source     = "./modules/instances"
+}
+
+module "storage" {
+  source     = "./modules/storage"
+}
+
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.4.0"
+
+    project_id   = var.project_id
+    network_name = "tf-vpc-366548"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = var.region
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = var.region
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        }
+    ]
+}
+
+resource "google_compute_firewall" "tf-firewall" {
+  name    = "tf-firewall"
+ network = "projects/qwiklabs-gcp-03-012087b62458/global/networks/tf-vpc-366548"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_tags = ["web"]
+  source_ranges = ["0.0.0.0/0"]
+}
